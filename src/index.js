a0e4ada26244fd8b3479d4927f7b3f8c1b0118ea
Lily Hughes-Robinson <oaguy1@gmail.com>
2023-07-16 00:26:39 -0400

    Make work local react app

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadURL("http://localhost:3000");
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
